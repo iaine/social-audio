26be22a7adc317817935ce2a43a3b198bc8797e2
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2024-02-26 09:40:21 +0000

    Adding zeeschuimer

--- load.py
+++ load.py
@@ -2,9 +2,12 @@
 Load data
 '''
 import requests
+from hashlib import sha256
+
+from database import database
 from social_exception import socialException
 
-class load():
+class Load():
 
     def load(self, fname):
         '''
@@ -35,4 +38,39 @@ class load():
                 a_file = audio_file.split('/')[:-1]
                 with open(directory + '/' + a_file, 'w') as fh:
                     fh.write(fname.text)
+
+    def load_zeeschuimer(self, fname):
+        '''
+        Handle the Zeeschuimer files ofr now
+        '''
+        _fname = fname.split('-')
+
+        if _fname[2] == "tiktok.com":
+            self._load_tiktok(fname)
+        elif _fname[2] == "instagram.com":
+            self._load_instagram(fname)
+        else:
+            raise socialException('Data Source not handled.')
+        
+    def _load_tiktok(fname):
+        '''
+        Handle Zeeschuimer export
+        '''
+
+        if fname.endswith('.csv'):
+            with open(fname, 'r') as fh:
+                data = fh.read()
+            
+            db = database()
+            project_id = self._create_hash(fname)
+            for datum in range(1, len(data)):
+                db.insert_row(project_id, datum[0])
+        else:
+            raise socialException('PLease convert with Zeehaven')
+        
+    def _create_hash(self, fname):
+        '''
+        Create hash for file
+        '''
+        return sha256(fname)
                 
